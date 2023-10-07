aa0344a6cb75a82c46ddfefc5ea97a71c62e2967
AlistairDriscoll <pallythecoder@gmail.com>
2023-10-07 11:51:32 +0100

    add icon and more metadata

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,110 +1,139 @@
-console.log("car bonnet");
 const welcomeContainer = document.getElementById("welcome-container");
 const gameContainer = document.getElementById("game-container");
 const resultsContainer = document.getElementById("results-container");
 const startGameBtn = document.getElementById("start-game-btn");
 const backToHomeBtn = document.getElementById("back-to-home-btn");
+const resultsHeader = document.getElementById("results");
+const backToGameBtn = document.getElementById("back-to-game-btn");
+const playerChoiceContainer = document.getElementById('player-choice');
+const computerChoiceContainer = document.getElementById('computer-choice');
+const options = document.getElementsByClassName('choices');
+let choices = {0: 'rock', 1: 'paper', 2: 'scissors', 3: 'lizard', 4: 'spock'}
+let result = "";
+let announcement = "";
 
-let playerOne = 0
-let playerTwo = 0
+backToGameBtn.addEventListener("click", showGame);
+startGameBtn.addEventListener("click", showGame);
 
+/**
+ * displays game section
+ * hides welcome and results section
+ */
 const showGame = function () {
   console.log("button start was clicked");
   welcomeContainer.classList.add("hide");
   gameContainer.classList.remove("hide");
+  resultsContainer.classList.add("hide");
+  result = "";
 };
 
-startGameBtn.addEventListener("click", showGame);
 
-//function to see which player wins
-
-let choices = {0: 'rock', 1: 'paper', 2: 'scissors', 3: 'lizard', 4: 'spock'}
-
+/**
+ *
+ */
 function gameFunction(playerMove) {
   let ranNum = Math.random() * 5;
   let compMove = Math.floor (ranNum);
-  console.log(choices[playerMove])
   let compChoice = choices[compMove];
   let playerChoice = choices[playerMove];
+  result, announcement = compareChoices(playerChoice, compChoice);
+  console.log(announcement);
   console.log(compChoice);
-  let result = compareChoices(playerChoice, compChoice);
-  console.log(result)
+  console.log(result);
+  resultsHeader.innerText = result;
+  gameContainer.classList.add("hide");
+  resultsContainer.classList.remove("hide");
+  playerChoiceContainer.innerText = playerChoice;
+  computerChoiceContainer.innerText = compChoice;
 }
 
 function compareChoices(op1, op2) {
-  console.log(op1, op2)
+  let announce = "";
   if (op1 == op2) {
-    return "draw";
+    announce = "It's a Draw!";
+    return "draw", announce;
   } else if ((op1 == "rock")) {
     if ((op2 == "paper")) {
-      return "loss"
+      announce = "Paper wraps rock!";
+      announce = "Paper wraps rock!";
+      return "loss", announce;
     } else if ((op2 == "scissors")) {
-      return "win";
+      announce = "Rock crushes scissors!";
+      return "win", announce;
     } else if ((op2 == "lizard")) {
-      return "win";
+      announce = "Rock crushes lizard!";
+      return "win", announce;
     } else {
-      return "loss";
+      announce = "Spock vaporises rock!";
+      announce = "Paper wraps rock!";
+      return "loss", announce;
     }
   } else if ((op1 = "paper")) {
     if ((op2 = "rock")) {
-      playerOne++;
+      announce = "Rock crushes lizard!";
+      return "win", announce;
     } else if ((op2 = "scissors")) {
-      playerTwo++;
+      announce = "Paper wraps rock!";
+      announce = "Paper wraps rock!";
+      return "loss", announce;
     } else if ((op2 = "lizard")) {
-      playerTwo++;
+      announce = "Paper wraps rock!";
+      return "loss", announce;;
     } else {
-      playerOne++;
+      announce = "Rock crushes lizard!";
+      return "win", announce;
     }
   } else if ((op1 = "scissors")) {
+    // analyse choices if scissors
     if ((op2 = "rock")) {
-      playerTwo++;
+      announce = "Paper wraps rock!";
+      return "loss", announce;;
     } else if ((op2 = "paper")) {
-      playerOne++;
+      announce = "Rock crushes lizard!";
+      return "win", announce;
     } else if ((op2 = "lizard")) {
-      playerOne++;
+      announce = "Rock crushes lizard!";
+      return "win", announce;
     } else {
-      playerTwo++;
+      announce = "Paper wraps rock!";
+      return "loss", announce;;
     }
   } else if ((op1 = "lizard")) {
     if ((op2 = "rock")) {
-      playerTwo++;
+      announce = "Paper wraps rock!";
+      return "loss", announce;;
     } else if ((op2 = "paper")) {
-      playerOne++;
+      announce = "Rock crushes lizard!";
+      return "win", announce;
     } else if ((op2 = "scissors")) {
-      playerTwo++;
+      announce = "Paper wraps rock!";
+      return "loss", announce;;
     } else {
-      playerOne++;
+      announce = "Rock crushes lizard!";
+      return "win", announce;
     }
   } else if (op1 = 'spock') {
     if ((op2 = "rock")) {
-      playerOne++;
+      announce = "Rock crushes lizard!";
+      return "win", announce;
     } else if ((op2 = "paper")) {
-      playerTwo++;
+      announce = "Paper wraps rock!";
+      return "loss", announce;;
     } else if ((op2 = "scissors")) {
-      playerOne++;
+      announce = "Rock crushes lizard!";
+      return "win", announce;
     } else {
-      playerTwo++;
+      announce = "Paper wraps rock!";
+      return "loss", announce;;
     }
   }
 }
 
-
-  let options = document.getElementsByClassName('choices');
   for (let i = 0; i < options.length; i++) {
     function eachOption() {
       gameFunction(i)
     }
       options[i].addEventListener('click', eachOption)
       let playerMove = options[i]
-      
-      
   }
 
-
-let scores = document.getElementById('scores-h');
-scores.innerHTML = `
-    <p>
-        Player One ${playerOne} : ${playerTwo} Player Two
-    </p>
-`
-let op1 = getOption1();
